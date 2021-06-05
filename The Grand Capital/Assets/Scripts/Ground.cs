beb79125da59a8c7ba1b8165521b5f901d6e19a3
qersha <a.secgeler.imp@gmail.com>
2021-06-05 18:25:00 +0300

    PlayerResource and Some Improvement Issue
    I did:
    Player Resource arrangement,
    ground things and improvement issues

---
+++ The Grand Capital/Assets/Scripts/Ground.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+	public GameObject improvementGameObject;
+
+	public bool isPlaced = false;
+
+	public Renderer rend;
+	Color groundBaseColor;
+	void Start()
+	{
+		rend = GetComponent<Renderer>();
+		groundBaseColor = rend.material.color;
+		rend.material.color = groundBaseColor;
+	}
+	public void OnMouseEnter()
+	{
+		rend.material.color = Color.grey;
+	}
+
+	public void OnMouseExit() 
+	{
+		rend.material.color = groundBaseColor;
+	}
+	public void OnMouseDown()
+	{
+		if (isPlaced==false)
+		{
+			isPlaced = true;
+			BuildImprovement();
+		}
+	}
+
+	public void BuildImprovement()
+	{
+		Instantiate(improvementGameObject, this.gameObject.transform.position, Quaternion.identity);
+	}
+}
---
+++ The Grand Capital/Assets/Scripts/PlayerResources.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResources : MonoBehaviour
+{
+	[Header("Player's Things")]
+	public float playerMoney;
+	public float playerRawMaterial;
+	public float playerProcessedMaterial;
+	public float playerProducts;
+	public float playerProcessedProducts;
+
+}
