72b9eca4e85c43d3d22cb4bb8681f80db90b75d2
qersha <a.secgeler.imp@gmail.com>
2021-06-15 01:38:36 +0300

    Worker Menu First Look

---
+++ The Grand Capital/Assets/Scripts/BuildingMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingMenu : MonoBehaviour
+{
+    [SerializeField] float border;
+
+	// This is building's workers menu
+	public GameObject buildingWorkerMenu;
+	public GameObject buildingMainMenu;
+	public GameObject workerButton;
+
+	// Update is called once per frame
+	void Start()
+	{
+		Vector3 menuPos = Camera.main.WorldToScreenPoint(this.transform.position);
+		
+		buildingMainMenu.transform.position = menuPos;
+	}
+	public void ShowBuildingsWorkers()
+	{
+		//When player click the "Workers" button, game display The Building's Workers Menu:
+		buildingWorkerMenu.SetActive(true);
+
+		//We have to deactive image componenet of Main Building Info Menu:
+		buildingMainMenu.GetComponent<Image>().enabled = false;
+		//Then we have to completely deactive The Button that we clicked to make all of those activities.
+		workerButton.SetActive(false);
+	}
+	void OnMouseEnter()
+	{
+		buildingMainMenu.SetActive(true);
+	}
+
+    void OnMouseExit()
+	{
+		buildingMainMenu.SetActive(false);
+		buildingWorkerMenu.SetActive(false);
+	}
+}
---
+++ The Grand Capital/Assets/Scripts/BuildingsWorkerMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingsWorkerMenu : MonoBehaviour
+{
+	// This is building's workers menu
+	public GameObject buildingWorkerMenu;
+	public GameObject buildingMainMenu;
+	public GameObject workerButton;
+	
+	public void ShowBuildingsWorkers()
+	{
+		//When player click the "Workers" button, game display The Building's Workers Menu:
+		buildingWorkerMenu.SetActive(true);
+
+		//We have to deactive image componenet of Main Building Info Menu:
+		buildingMainMenu.GetComponent<Image>().enabled = false;
+		//Then we have to completely deactive The Button that we clicked to make all of those activities.
+		workerButton.SetActive(false);
+	}
+	
+}
